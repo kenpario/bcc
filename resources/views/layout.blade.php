016be0f4b05b1cbed86805cecff99e17e0a46754
kenpario <108219125+kenpario@users.noreply.github.com>
2025-08-13 15:35:57 +0300

    Layout update
    Changed the dropdown menu to stay hidden at refresh and added new item button on the table.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -33,10 +33,13 @@
 
     <nav class="bg-gray-200 border-gray-200 dark:border-gray-600 dark:bg-gray-800 m-2 rounded-lg shadow-lg">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+            <!-- Logo -->
             <a href="https://flowbite.com" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BCC</span>
             </a>
+
+            <!-- Mobile menu button -->
             <button data-collapse-toggle="mega-menu-full" type="button"
                 class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 aria-controls="mega-menu-full" aria-expanded="false">
@@ -47,7 +50,9 @@
                         d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
             </button>
-            <div id="mega-menu-full" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+
+            <!-- Main menu (collapsed by default) -->
+            <div id="mega-menu-full" class="hidden items-center justify-between w-full md:flex md:w-auto md:order-1">
                 <ul class="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
                     <li>
                         <a href="#"
@@ -56,104 +61,130 @@
                     </li>
                     <li>
                         <button id="mega-menu-full-dropdown-button" data-collapse-toggle="mega-menu-full-dropdown"
-                            class="flex items-center justify-between w-full py-2 px-3 font-medium text-gray-900 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Company
+                            class="flex items-center justify-between w-full py-2 px-3 font-medium text-gray-900 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                            Company
                             <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                 fill="none" viewBox="0 0 10 6">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                     stroke-width="2" d="m1 1 4 4 4-4" />
-                            </svg></button>
+                            </svg>
+                        </button>
                     </li>
                     <li>
                         <a href="#"
-                            class="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Marketplace</a>
+                            class="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                            Marketplace
+                        </a>
                     </li>
                     <li>
                         <a href="#"
-                            class="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Resources</a>
+                            class="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                            Resources
+                        </a>
                     </li>
                     <li>
                         <a href="#"
-                            class="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                            class="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                            Contact
+                        </a>
                     </li>
                 </ul>
             </div>
         </div>
+
+        <!-- Dropdown menu (collapsed by default) -->
         <div id="mega-menu-full-dropdown"
-            class="mt-1 bg-white border-gray-200 shadow-xs border-y dark:bg-gray-800 dark:border-gray-600">
+            class="hidden mt-1 bg-white border-gray-200 shadow-xs border-y dark:bg-gray-800 dark:border-gray-600">
             <div
                 class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:grid-cols-3 md:px-6">
+
+                <!-- Column 1 -->
                 <ul aria-labelledby="mega-menu-full-dropdown-button">
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
                             <div class="font-semibold">Online Stores</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that
-                                you're already using.</span>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">
+                                Connect with third-party tools that you're already using.
+                            </span>
                         </a>
                     </li>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
                             <div class="font-semibold">Segmentation</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that
-                                you're already using.</span>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">
+                                Connect with third-party tools that you're already using.
+                            </span>
                         </a>
                     </li>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
                             <div class="font-semibold">Marketing CRM</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that
-                                you're already using.</span>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">
+                                Connect with third-party tools that you're already using.
+                            </span>
                         </a>
                     </li>
                 </ul>
+
+                <!-- Column 2 -->
                 <ul>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
                             <div class="font-semibold">Online Stores</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that
-                                you're already using.</span>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">
+                                Connect with third-party tools that you're already using.
+                            </span>
                         </a>
                     </li>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
                             <div class="font-semibold">Segmentation</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that
-                                you're already using.</span>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">
+                                Connect with third-party tools that you're already using.
+                            </span>
                         </a>
                     </li>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
                             <div class="font-semibold">Marketing CRM</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that
-                                you're already using.</span>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">
+                                Connect with third-party tools that you're already using.
+                            </span>
                         </a>
                     </li>
                 </ul>
+
+                <!-- Column 3 -->
                 <ul class="hidden md:block">
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
                             <div class="font-semibold">Audience Management</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that
-                                you're already using.</span>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">
+                                Connect with third-party tools that you're already using.
+                            </span>
                         </a>
                     </li>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
                             <div class="font-semibold">Creative Tools</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that
-                                you're already using.</span>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">
+                                Connect with third-party tools that you're already using.
+                            </span>
                         </a>
                     </li>
                     <li>
                         <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
                             <div class="font-semibold">Marketing Automation</div>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that
-                                you're already using.</span>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">
+                                Connect with third-party tools that you're already using.
+                            </span>
                         </a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
+
 
     <main>
         @yield('content')
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,6 +3,14 @@
 
 
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg m-1">
+        <div class="flex justify-end">
+            <a href="">
+                <button type="button"
+                    class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 m-2 shadow-lg"><i
+                        class="fa fa-plus-circle" aria-hidden="true"></i> New
+                    Item</button>
+            </a>
+        </div>
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
