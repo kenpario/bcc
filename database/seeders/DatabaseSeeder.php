f5acfd09159e5fb758ffd7314d6f76da816e877d
kenpario <108219125+kenpario@users.noreply.github.com>
2025-08-19 13:12:11 +0300

    Factory
    Created a factory and a seed function to easily make the groups as they should be so that the application works properly

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Group;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -8,4 +9,10 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+
+    public function run()
+{
+    Group::create(['name' => 'Administrator']);
+    Group::create(['name' => 'Member']);
+}
 }
